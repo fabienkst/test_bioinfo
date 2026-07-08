4897360ea141125bac33b91779fa7e0a3f694724
Fabien <fabien.kst@gmail.com>
2026-07-08 22:35:21 +0200

    adding standard template python scripts

---
+++ python/plots.py
@@ -0,0 +1,29 @@
+#!/Users/ggw_974_1/.pyenv/shims/python
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as npc
+import argparse
+import os
+import logging
+
+def save_figure(plt, outfile_prefix, ext="pdf"):
+	file = f"{outfile_prefix}.{ext}"
+	plt.savefig(file, format=ext)
+
+def read_data(input_data):
+	dt = pd.read_table(input_data, sep="\t")
+	print(dt)
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(prog="plots.py", description="Script for designing plots")
+	mandatory = parser.add_argument_group('Mandatory options.')
+	mandatory.add_argument('--input', '-i', required=True, help="Input data.")
+	mandatory.add_argument('--output', '-o', required=True, help="Output path for results.")
+
+	args = parser.parse_args()
+
+	pd.set_option("display.max_columns", None)
+	pd.set_option("display.max_colwidth",None)
+	read_data(args.input)
---
+++ python/test.py
@@ -0,0 +1,21 @@
+#!/Users/ggw_974_1/.pyenv/shims/python
+
+import os
+import argparse
+import re
+
+def test_calcul(input_data):
+    new_input = int(input_data)*2
+    return str(new_input)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="Test", description="Main test program")
+    mandatory = parser.add_argument_group("Mandatory options")
+    mandatory.add_argument("--input", "-i", dest="input", required=True, help="Required input data")
+
+    args = parser.parse_args()
+
+    print ("Welcome ! \n" + args.input)
+    print("Processing...")
+    New_data = test_calcul(args.input)
+    print("Output is \n" + New_data)
